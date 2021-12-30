1d4fc8599e2d4c8b98699f796e7d7942a51f14e6
Alba A.C <albazcano@gmail.com>
2021-12-30 18:20:47 +0100

    modified reto3.php

---
+++ Reto3.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reto3</title>
+
+</head>
+<body>
+    <a href="index.php">Volver</a>
+    <h1>Usuarios Biblioteca</h1>
+    
+    <?php
+        $userInfo = [
+            "Nombre" => "Alba",
+            "Edad" => "37",
+            "Ciudad" => "Gijón",
+            "Teléfono" => "666.666.666",
+            "Email" => "alba@gmail.com"
+        ];
+        $tableHTML = "<table>" . PHP_EOL . "<thead>" . "<tr>";
+
+        foreach ($userInfo as $clave=>$valor)
+   		{
+            $tableHTML.="<th>$clave</th>";
+            
+        } $tableHTML.="</tr>" . "</thead>" ;
+        foreach ($userInfo as $clave=>$valor)
+   		{
+               $tableHTML.="<td>$valor</td>" . PHP_EOL;
+        }    
+       
+           $tableHTML.="</tr>" . "</table>";
+
+        echo $tableHTML;
+	
+      
+    ?>
+
+
+
+</body>
+</html>
